5c4bdc39bb85c743a7e9c5c1f61c693f7d98d93e
MrZorg111 <yodji333@mail.ru>
2022-06-06 22:58:03 +0300

    Solution 1

--- main.cpp
+++ main.cpp
@@ -2,28 +2,36 @@
 
 int skok (int n, int s = 2) {
     int st_1 = 1, sp = 0, sol = 1;
-    if ((sp += st_1) == n) {
+    //std::cout << sol << "\n";
+    if ((sp += st_1) == n) { // 0 += 1 < 4
         sol++;
         sp = 0;
+        //std::cout << sol;
         return sol + skok(n, s + 1);
     }
-    if ((sp += s) == n) {
+    //std::cout << "Suda!\n";
+    if ((sp += s) == n) { // 1 += 2 < 4
         sol++;
         sp = 0;
+        //std::cout << "Suda!\n";
         return sol + skok(n, s + 1);
     }
+
     if (sp > n) {
         st_1++;
+        //std::cout << "Suda!\n";
         return skok(n, s = 1);
     }
     if (st_1 == 3 && s == 3) {
+        //std::cout << "Suda!\n";
         return sol;
     }
+
 }
 
 int main() {
-    int n;
+    int sd;
     std::cout << "Enter number\n";
-    std::cin >> n;
-    std::cout << skok(n);
+    std::cin >> sd;
+    std::cout << skok(sd);
 }
