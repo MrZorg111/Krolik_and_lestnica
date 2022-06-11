1bc0e39a808edfb789fd7dcc5bd1b79ba854c065
MrZorg111 <yodji333@mail.ru>
2022-06-11 11:44:31 +0300

    Вариант для нахождения всех способов достич нужной ступеньки.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,20 @@
 #include <iostream>
 
-int skok (int n, int s = 2) {
-    int st_1 = 1, sp = 0, sol = 1;
-    if ((sp += st_1) == n) {
-        sol++;
-        sp = 0;
-        return sol + skok(n, s + 1);
+int jump (int dest_p, int sol = 2) {
+    if (dest_p == 0 || dest_p == 1) {
+        return 1;
     }
-    if ((sp += s) == n) {
-        sol++;
-        sp = 0;
-        return sol + skok(n, s + 1);
-    }
-    if (sp > n) {
-        st_1++;
-        return skok(n, s = 1);
-    }
-    if (st_1 == 3 && s == 3) {
+    if(dest_p == 2) {
         return sol;
     }
+    jump(dest_p - 1, sol * 2);
+
 }
 
 int main() {
     int n;
     std::cout << "Enter number\n";
     std::cin >> n;
-    std::cout << skok(n);
+    std::cout << jump(n);
+
 }
